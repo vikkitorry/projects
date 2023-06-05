da220a4e146a7d6bc66b51247103358b6387e20a
vikkitorry <vikkitorry6716@gmail.com>
2023-06-05 18:05:57 +1000

    refactor: migrate 'sources.js' to TS

---
+++ migration-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import {Source} from '../../../types/index';
+
+class Sources {
+    draw(data : Array<Source>) : void {
+        const fragment = document.createDocumentFragment() as DocumentFragment;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
+    }
+}
+
+export default Sources;
