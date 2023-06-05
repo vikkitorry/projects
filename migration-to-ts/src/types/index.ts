85102dcedb3c4fdc859389a85fa8c16f204b93c9
vikkitorry <vikkitorry6716@gmail.com>
2023-06-05 17:21:58 +1000

    feat: add interfaces

---
+++ migration-to-ts/src/types/index.ts
@@ -0,0 +1,40 @@
+export enum Endpoints {
+  Everything = 'everything',
+  Sources = 'sources',
+}
+
+export interface Options {
+  apiKey: string;
+  sources: string;
+};
+
+export interface Article {
+  source: {
+    id: string;
+    name: string
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+export interface Source {
+  category: string;
+  language: string;
+  country: string;
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface ArticleData {
+  status: string;
+  totalResults: number;
+  articles: Array<Article>;
+  sources: Array<Source>;
+}
