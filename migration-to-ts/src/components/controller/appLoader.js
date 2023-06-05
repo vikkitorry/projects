1993ded2a253ba8cb3f1042a3164ae0fbe10143c
vikkitorry <vikkitorry6716@gmail.com>
2023-06-05 10:35:23 +1000

    feat: set up configs for webpack, TS..

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '928a11df0183461780a8711ffc0086d8', // получите свой ключ https://newsapi.org/
         });
     }
 }
