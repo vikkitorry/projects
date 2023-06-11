1c7e0aa2d9f03cbed4321a165778e944076c5348
vikkitorry <vikkitorry6716@gmail.com>
2023-06-11 19:15:36 +1000

    refactor: change link

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: 'aff0c18826094d6398035e9f7cc5f1d2', // получите свой ключ https://newsapi.org/
     });
   }
